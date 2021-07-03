6d4bb69f713c23816ccd7e327202cc4a762eb08f
vagrant <vagrant@homestead>
2021-07-03 03:42:27 +0000

    passport導入

---
+++ app/Http/Controllers/SpaController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+
+class SpaController extends Controller
+{
+//    /**
+//     * auth middleware通す
+//     * SpaController constructor.
+//     */
+//    public function __construct(){
+//        $this->middleware('auth');
+//    }
+
+    /**
+     * SPA
+     * @param string $path
+     * @return Application|Factory|View
+     */
+//    public function index($path = 'daily-report-select')
+    public function index()
+    {
+//        $regex = '/\A[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[0-5][a-fA-F0-9]{3}-[089aAbB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}\z/';
+//        if ((bool) preg_match($regex, basename($path))) {
+//            $path = dirname($path);
+//        }
+//
+//        if (request()->user()->role === config('auth.roles.general_user')) {
+//            if (! in_array($path, config('routes.general_user'))) {
+//                abort(404);
+//            }
+//        }
+
+        return view('app');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 //    return view('app');
 //});
 
+Route::view('login', 'app')->name('login');
+Route::post('login', [\App\Http\Controllers\Auth\LoginController::class,'login']);
+Route::get('logout', [\App\Http\Controllers\Auth\LoginController::class,'logout']);
+
 Route::get('/{any}', function() {
     return view('app');
 })->where('any', '.*');
