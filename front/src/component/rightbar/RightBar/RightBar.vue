89eff3a53819667fee1b9aa6f51fb2701681d58d
twatanabe <twatanabe@fourmix.co.jp>
2021-07-17 14:29:55 +0900

    fix mock

--- front/src/component/rightbar/RightBar/RightBar.vue
+++ front/src/component/rightbar/RightBar/RightBar.vue
@@ -1,9 +1,6 @@
 <template>
 <div>
     <div>
-        <v-input placeholder="キーワード検索"></v-input>
-    </div>
-    <div>
         <NowDoing></NowDoing>
     </div>
     <div>
@@ -16,13 +13,12 @@
 </template>
 
 <script>
-import VInput from "@/component/form/VInput/VInput";
 import NowDoing from "@/component/rightbar/NowDoing/NowDoing";
 import RecommendUser from "@/component/rightbar/ReccomendUser/RecommendUser";
 import RecommendTopic from "@/component/rightbar/ReccomendTopic/RecommendTopic";
 export default {
     name: "RightBar",
-    components: {RecommendTopic, RecommendUser, NowDoing, VInput}
+    components: {RecommendTopic, RecommendUser, NowDoing}
 }
 </script>
 
--- front/src/pages/tweet/Tweet.vue
+++ front/src/pages/tweet/Tweet.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-8">
-                <tweet-list></tweet-list>
+            <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12">
+                <tweet-list class="scroll"></tweet-list>
             </div>
-            <div class="col-4">
-                <right-bar></right-bar>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                <v-input placeholder="キーワード検索"></v-input>
+                <right-bar class="scroll"></right-bar>
             </div>
         </div>
     </div>
@@ -14,12 +15,16 @@
 <script>
 import TweetList from "@/component/tweet/tweetList/TweetList";
 import RightBar from "@/component/rightbar/RightBar/RightBar";
+import VInput from "@/component/form/VInput/VInput";
 export default {
 name: "Tweet",
-    components: {RightBar, TweetList}
+    components: {VInput, RightBar, TweetList}
 }
 </script>
 
 <style scoped>
-
+.scroll{
+    height: 100vh;
+    overflow: auto;
+}
 </style>
