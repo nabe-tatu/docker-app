9e5be6276d5edba65c7fac1780353a557b3659c6
nabe-tatu <watanabetatsuya0906@gmail.com>
2021-09-25 13:32:43 +0900

    prod

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,17 +1,1 @@
-<!DOCTYPE html>
-<html lang="">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <link rel="icon" href="/app/favicon.ico">
-    <title>frontend</title>
-  <link href="/app/js/chunk-vendors.js" rel="preload" as="script"><link href="/app/js/index.js" rel="preload" as="script"></head>
-  <body>
-    <noscript>
-      <strong>We're sorry but frontend doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
-    </noscript>
-    <div id="app"></div>
-    <!-- built files will be auto injected -->
-  <script type="text/javascript" src="/app/js/chunk-vendors.js"></script><script type="text/javascript" src="/app/js/index.js"></script></body>
-</html>
+<!DOCTYPE html><html lang=""><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="icon" href="/app/favicon.ico"><title>frontend</title><link href="/app/css/index.fb0c6e1c.css" rel="preload" as="style"><link href="/app/js/chunk-vendors.8aa1a4ab.js" rel="preload" as="script"><link href="/app/js/index.0794c7cb.js" rel="preload" as="script"><link href="/app/css/index.fb0c6e1c.css" rel="stylesheet"></head><body><noscript><strong>We're sorry but frontend doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id="app"></div><script src="/app/js/chunk-vendors.8aa1a4ab.js"></script><script src="/app/js/index.0794c7cb.js"></script></body></html>
